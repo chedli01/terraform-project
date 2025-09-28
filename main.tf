db0064cd3a8cb3b02f2e1e7f96b44a6c0cb3289f
chedli01 <chedli.masmoudi01@gmail.com>
2025-09-28 18:48:54 +0100

    simulate a microservices app

--- main.tf
+++ main.tf
@@ -4,12 +4,41 @@ terraform {
       source  = "hashicorp/local"
       version = "~> 2.4"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
   }
 }
 
 provider "local" {}
+provider "null" {}
 
-resource "local_file" "myfile" {
-  content  = var.content
-  filename = "${path.module}/myfile.txt"
+
+resource "local_file" "db_config"{
+  filename="${path.module}/db_config.txt"
+  content=<<EOT
+DB_HOST=localhost
+DB_USER=${var.db_user}
+DB_PASS=${var.db_pass}
+  EOT
+}
+
+resource "local_file" "backend"{
+  filename="${path.module}/backend_service.txt"
+  content="Backend (Node) is connected to db : ${local_file.db_config.filename}"
+  depends_on = [local_file.db_config]
+}
+
+resource "local_file" "frontend" {
+  filename = "${path.module}/frontend_service.txt"
+  content  = "Frontend talks to Backend : ${local_file.backend.filename}"
+  depends_on = [local_file.backend]
+
 } 
+
+resource "null_resource" "deploy_message" {
+  provisioner "local-exec" {
+    command = "echo '✅ Fake microservices infra deployed successfully!'"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,15 @@ variable "content"{
   type    = string
   default = "hello this is chedli and this is the content of my file v2" 
 }
+
+variable "db_user" {
+  description = "Database username"
+  type        = string
+  default     = "admin"
+}
+
+variable "db_pass" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
